acf0818ebe471dd4ac30872da6d35d4bb835f599
dani <dani@mandarine.(none)>
2009-04-23 13:41:31 +0200

    encustar

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,13 +1,24 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
 
+  SINO = ['s', 'n']
+  TAN = ['todas', 'alguna', 'ninguna']
+  
+  EDIFICIO_PERIODO = ['antes1950', 'de1951a1960', 'de1961a1970', 'de1971a1980', 'despues1981']
+
+  VIVIENDA_TIPO_EDIFICIO = ['bloque', 'aislado', 'adosado', 'pareado']
+  VIVIENDA_PLANTA = ['planta_baja', 'planta_intermedia', 'planta_ultima']
+  VIVIENDA_TEJADO = ['tejado_plano', 'tejado_inclinado']
+  VIVIENDA_REFORMAS = %w(aistejado aisparedes toldos doblevidrio asisuelo)
+  VIVIENDA_REFORMA_ULTIMA = ['menos5', 'entre5y10', 'mas10']
+
   EQUIP_SERVICIOS = %w(calef aire agua)
   EQUIP_FUENTES = %w(electr gasnat gasoleo gasbutprop solar madera otros)
 
-  EDIFICIO_PERIODO = ['antes1950', 'de1951a1960', 'de1961a1970', 'de1971a1980', 'despues1981']
-  VIVIENDA_TIPO_EDIFICIO = ['bloque', 'aislado', 'adosado', 'pareado']
-  VIVIENDA_PLANTA = ['planta_baja', 'planta_intermedia', 'planta_ultima']
-
-  VIVIENDA_REFORMAS = %w(aistejado aisparedes toldos doblevidrio asisuelo)
-  VIVIENDA_REFORMA_ULTIMA = ['menos5', 'entre5y10', 'mas10']
+  EQUIP_AGUA_TIPO = ['termo', 'caldera', 'bomba']
+  EQUIP_AGUA_CALDERA_TIPO =['individual', 'centralizada']
+  EQUIP_DISPOSITIVOS = ['lavadora', 'lavavajillas', 'frigorífico', 'hornoel' 'hornonoel', 'microondas',
+    'television', 'ordenador', 'portatil', 'secadora', 'congelador']
+  EQUIP_ILUMINACION = ['incan', 'bajocon', 'fluor']
+  EQUIP_AC_DONDE = ['salas', 'dormitorios']
 end
--- db/migrate/20090422172020_profile_equipamiento.rb
+++ db/migrate/20090422172020_profile_equipamiento.rb
@@ -6,10 +6,37 @@ class ProfileEquipamiento < ActiveRecord::Migration
       end
     end
 
-    add_column :profiles, :equip_agua_caliente, :boolean
+    add_column :profiles, :equip_agua_caliente, :string, :limit => 3
     add_column :profiles, :equip_agua_tipo, :string, :limit => 8
-    add_column :profiles, :equip_agua_dispositivo, :string, :limit => 8
+    add_column :profiles, :equip_agua_solar, :string, :limit => 3
+    add_column :profiles, :equip_agua_edad, :integer, :limit => 3
+    add_column :profiles, :equip_agua_caldera_tipo, :string, :limit => 8
+    add_column :profiles, :equip_agua_caldera_acumulacion, :string, :limit => 3
+
+    add_column :profiles, :equip_calefaccion, :string, :limit => 3
+    add_column :profiles, :equip_calefaccion_tipo, :string, :limit => 8
+    add_column :profiles, :equip_calefaccion_edad, :integer, :limit => 3
+    add_column :profiles, :equip_calefaccion_misma, :string, :limit => 3
+    add_column :profiles, :equip_calefaccion_caldera_tipo, :string, :limit => 8
+    add_column :profiles, :equip_calefaccion_bomba_tipo, :string, :limit => 8
+
+
+    add_column :profiles, :equip_ac, :string, :limit => 3
+    Profile::EQUIP_AC_DONDE.each do |donde|
+      add_column :profiles, "equip_ac_donde_#{donde}", :string, :limit => 8
+    end
+    add_column :profiles, :equip_ac_tipo, :string, :limit => 8
+    add_column :profiles, :equip_ac_edad, :integer, :limit => 3
+
+    Profile::EQUIP_DISPOSITIVOS.each do |disp|
+      add_column :profiles, "equip_disp_tiene_#{disp}", :integer
+    end
+
+    Profile::EQUIP_ILUMINACION.each do |disp|
+      add_column :profiles, "equip_ilum_tiene_#{disp}", :integer
+    end
   end
+  
 
   def self.down
     Profile::EQUIP_SERVICIOS.each do |serv|
@@ -20,6 +47,33 @@ class ProfileEquipamiento < ActiveRecord::Migration
 
     remove_column :profiles, :equip_agua_caliente
     remove_column :profiles, :equip_agua_tipo
-    remove_column :profiles, :equip_agua_dispositivo
+    remove_column :profiles, :equip_agua_solar
+    remove_column :profiles, :equip_agua_edad
+    remove_column :profiles, :equip_agua_caldera_tipo
+    remove_column :profiles, :equip_agua_caldera_acumulacion
+
+    remove_column :profiles, :equip_calefaccion
+    remove_column :profiles, :equip_calefaccion_tipo
+    remove_column :profiles, :equip_calefaccion_edad
+    remove_column :profiles, :equip_calefaccion_misma
+    remove_column :profiles, :equip_calefaccion_caldera_tipo
+    remove_column :profiles, :equip_calefaccion_bomba_tipo
+
+    remove_column :profiles, :equip_ac, :string
+    Profile::EQUIP_AC_DONDE.each do |donde|
+      remove_column :profiles, "equip_ac_donde_#{donde}", :string, :limit => 8
+    end
+    remove_column :profiles, :equip_ac_tipo
+    remove_column :profiles, :equip_ac_edad
+
+    Profile::EQUIP_DISPOSITIVOS.each do |disp|
+      remove_column :profiles, "equip_disp_tiene_#{disp}"
+    end
+
+    Profile::EQUIP_ILUMINACION.each do |disp|
+      remove_column :profiles, "equip_ilum_tiene_#{disp}"
+    end
+
+
   end
 end
